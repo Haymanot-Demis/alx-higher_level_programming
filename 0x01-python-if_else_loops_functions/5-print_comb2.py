0d5659bc1a1c5e696203a65e53bd10fcaeabce0f
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-11-18 09:28:56 +0300

    pycodestyle

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(0,100):
     if i < 99:
-        print("{:03d}".format(i),end=', ')
+        print("{:02d}".format(i), end=', ')
     else:
-        print("{:0d}".format(i))
+        print("{:02d}".format(i))
+        
